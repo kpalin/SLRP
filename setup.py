5bae0fa7b656ff83324b2b8c22ec1c4872b13393
Kimmo Palin <kimmo.palin@gmail.com>
2011-03-07 10:39:01 +0000

    Slightly better versioning system based on git commit identifiers.

--- setup.py
+++ setup.py
@@ -3,8 +3,24 @@
 from distutils.core import setup
 import subprocess as sp
 
+try:
+    git_proc = sp.Popen(["git","describe"],stdout=sp.PIPE)
+    git_version = git_proc.stdout.read().strip()
+    git_proc.wait()
+    if git_proc.returncode == 0:
+        print "Successfull git call"
+        open("SLRPlib/__version__.py","w").write("_version=\"%s\""%(git_version))
+except OSError:
+    pass
+                            
+
+from SLRPlib.__version__ import _version
+
+
+
+
 setup(name='SLRP',
-      version="$Id$,
+      version=_version,
       description='Systematic Long Range Phasing',
       author='Kimmo Palin',
       author_email='kimmo.palin@sanger.ac.uk',
