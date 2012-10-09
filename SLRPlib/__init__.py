946bbcd9ea11a6237475e4224086080e38e392b3
Kimmo Palin <kimmo.palin@gmail.com>
2012-10-09 08:58:04 +0300

    Eased dependence on git

--- SLRPlib/__init__.py
+++ SLRPlib/__init__.py
@@ -1,1 +1,5 @@
-from version import __version__
+try:
+    from version import __version__
+except ImportError:
+    # In case we haven't correctly created the version file (e.g. missing git)
+    __version__="1.0-nogit"
